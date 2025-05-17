19efd7e1d77ab5eeb18edc163d3751267d72e558
0lddMann <ijao9.olpindo@gmail.com>
2025-05-17 12:06:37 +0800

    Delete newarrivalarrow.js

--- newarrivalarrow.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.arrivals-arrow.left').onclick = function() {
-    document.querySelector('.arrivals-list').scrollBy({ left: -240, behavior: 'smooth' });
-};
-document.querySelector('.arrivals-arrow.right').onclick = function() {
-    document.querySelector('.arrivals-list').scrollBy({ left: 240, behavior: 'smooth' });
-};
